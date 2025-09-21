f2cb55b5256dd28705ef19c487ffd3ff352bbec0
vitoriab010 <vitoriakarina91@gmail.com>
2025-09-21 18:09:14 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 document.addEventListener("click", function(event) {
     const isClickInsideGrid = event.target.closest(".conteudoCont > div");
     
-    // Se o clique NÃO foi dentro de um dos grids
+    // se o clique NÃO foi dentro de um dos grids
     if (!isClickInsideGrid) {
-        // Remove a classe 'active' de todos os grids
+        // remove a classe "active" de todos os grids
         document.querySelectorAll(".conteudoCont > div")
             .forEach(div => div.classList.remove("active"));
     }
@@ -11,15 +11,16 @@ document.addEventListener("click", function(event) {
 
 document.querySelectorAll(".conteudoCont > div").forEach(item => {
     item.addEventListener("click", function(event) {
-        // Impede que o clique no grid ative o evento do body
+        // impede que o clique no grid ative o evento do body
         event.stopPropagation();
         
-        // Remove a classe 'active' de todos os grids
+        // remove a classe "ctive" de todos os grids
         document.querySelectorAll(".conteudoCont > div")
             .forEach(div => div.classList.remove("active"));
         
-        // Adiciona a classe 'active' apenas no grid clicado
+        // adiciona a classe 'active' apenas no grid clicado
         this.classList.add("active");
     });
 });
+
 
