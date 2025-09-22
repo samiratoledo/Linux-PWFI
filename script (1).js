fe6d4c95a98b117d8177b3b08ca774017bdf0421
Eddie <eduardaalcsantos@gmail.com>
2025-09-22 18:43:06 -0300

    Add files via upload

---
+++ script (1).js
@@ -0,0 +1,23 @@
+document.addEventListener("click", function(event) {
+    const isClickInsideGrid = event.target.closest(".conteudoCont > div");
+    
+    // se o clique NÃO foi dentro de um dos grids
+    if (!isClickInsideGrid) {
+        // remove a classe 'active' de todos os grids
+        document.querySelectorAll(".conteudoCont > div")
+            .forEach(div => div.classList.remove("active"));
+    }
+});
+
+document.querySelectorAll(".conteudoCont > div").forEach(item => {
+    item.addEventListener("click", function(event) {
+        event.stopPropagation();
+        
+        // remove a classe 'ctive' de todos os grids
+        document.querySelectorAll(".conteudoCont > div")
+            .forEach(div => div.classList.remove("active"));
+        
+        // adiciona a classe 'active' apenas no grid clicado
+        this.classList.add("active");
+    });
+});
